d2fc0c39afa889bc1243d760a57c240e9e30dd6a
Dario George <edu.dariogeorge21@gmail.com>
2024-12-19 08:12:36 +0530

    Important Questions

---
+++ join and split.py
@@ -0,0 +1,17 @@
+##JOIN
+l=["Hello","I am","learning","Python"]
+str1=" ".join(l)
+print(str1)
+
+#OUTPUT
+#Hello I am learning Python
+
+
+##SPLIT
+
+str1="Hello I am learning Python"
+l=str1.split(" ")
+print(l)
+
+#OUTPUT
+#["Hello","I am","learning","Python"]
